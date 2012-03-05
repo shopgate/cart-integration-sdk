079262e529e4a91fa146bbcac07a748035acd07e
mueller <mueller@e652e40c-185c-4641-87fb-3ecfe257c510>
2012-03-05 15:39:04 +0000

    Auf relative Includes umgestellt
    git-svn-id: https://svn.localdev.cc/svn/shopgate_plugin_library/trunk@5 e652e40c-185c-4641-87fb-3ecfe257c510

--- shopgate.php
+++ shopgate.php
@@ -1,11 +1,11 @@
 <?php
 if (!defined('DS')) define('DS', '/');
 
-require_once('classes'.DS.'core.php');
-require_once('classes'.DS.'customers.php');
-require_once('classes'.DS.'orders.php');
+require_once(dirname(__FILE__).DS.'classes'.DS.'core.php');
+require_once(dirname(__FILE__).DS.'classes'.DS.'customers.php');
+require_once(dirname(__FILE__).DS.'classes'.DS.'orders.php');
 
-require_once('vendors'.DS.'2d_is.php');
-require_once('vendors'.DS.'JSON.php');
-require_once('vendors'.DS.'mobile_redirect.class.php');
+require_once(dirname(__FILE__).DS.'vendors'.DS.'2d_is.php');
+require_once(dirname(__FILE__).DS.'vendors'.DS.'JSON.php');
+require_once(dirname(__FILE__).DS.'vendors'.DS.'mobile_redirect.class.php');
 
