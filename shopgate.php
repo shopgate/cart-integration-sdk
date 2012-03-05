95b49210a57c43face460c79e15c5a116cdc3e63
mueller <mueller@e652e40c-185c-4641-87fb-3ecfe257c510>
2012-03-05 15:50:50 +0000

    Config inkludiert
    git-svn-id: https://svn.localdev.cc/svn/shopgate_plugin_library/trunk@6 e652e40c-185c-4641-87fb-3ecfe257c510

--- shopgate.php
+++ shopgate.php
@@ -9,3 +9,16 @@ require_once(dirname(__FILE__).DS.'vendors'.DS.'2d_is.php');
 require_once(dirname(__FILE__).DS.'vendors'.DS.'JSON.php');
 require_once(dirname(__FILE__).DS.'vendors'.DS.'mobile_redirect.class.php');
 
+try {
+	$config = ShopgateConfig::validateAndReturnConfig();
+	
+	$plugin = $config["plugin"];
+	
+	if(file_exists(SHOPGATE_BASE_DIR.'/plugins/'.$plugin)
+	&& file_exists(SHOPGATE_BASE_DIR.'/plugins/'.$plugin.'/includes.php')) {
+		include_once SHOPGATE_BASE_DIR.'/plugins/'.$plugin.'/includes.php';
+	}
+	
+} catch (Exception $e) {
+	
+}
