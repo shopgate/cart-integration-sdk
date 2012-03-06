aa18b1d8ea55a8db8e1ea122e5efb9c050f7ce05
mueller <mueller@e652e40c-185c-4641-87fb-3ecfe257c510>
2012-03-06 12:19:26 +0000

    Anstatt includes.php wird die shopgate.php inkludiert.
    git-svn-id: https://svn.localdev.cc/svn/shopgate_plugin_library/trunk@12 e652e40c-185c-4641-87fb-3ecfe257c510

--- vendors/mobile_redirect.class.php
+++ vendors/mobile_redirect.class.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once dirname(__FILE__).'/../includes.php';
+include_once dirname(__FILE__).'/../shopgate.php';
 
 /**
  * Manage and create the JavaScript for redirect to mobile-site
