1c5d745f502cb411ca731fe436d80fff7c5d36af
mueller <mueller@e652e40c-185c-4641-87fb-3ecfe257c510>
2012-03-06 08:51:49 +0000

    Config Datei der Library hinzugefügt
    git-svn-id: https://svn.localdev.cc/svn/shopgate_plugin_library/trunk@7 e652e40c-185c-4641-87fb-3ecfe257c510

---
+++ config/config.php
@@ -0,0 +1,23 @@
+<?php
+
+$shopgate_config = array();
+$shopgate_config["customer_number"] = "12345";
+$shopgate_config["shop_number"] = "12345";
+$shopgate_config["apikey"] = "abcdefg";
+$shopgate_config["plugin"] = "example";
+$shopgate_config["enable_ping"] = true;
+$shopgate_config["enable_get_shop_info"] = true;
+$shopgate_config["enable_http_alert"] = true;
+$shopgate_config["enable_connect"] = true;
+$shopgate_config["enable_get_items_csv"] = true;
+$shopgate_config["enable_get_reviews_csv"] = true;
+$shopgate_config["enable_get_pages_csv"] = true;
+$shopgate_config["enable_get_log_file"] = true;
+$shopgate_config["enable_mobile_website"] = true;
+$shopgate_config["generate_items_csv_on_the_fly"] = true;
+$shopgate_config["max_attributes"] = 50;
+$shopgate_config["use_custom_error_handler"] = false;
+$shopgate_config["use_stock"] = false;
+$shopgate_config["shop_is_active"] = false;
+$shopgate_config["background_color"] = "#333";
+$shopgate_config["foreground_color"] = "#3d3d3d";
