988ad9418343937ed3cc7e6776d352cdf2430391
weber <weber@e652e40c-185c-4641-87fb-3ecfe257c510>
2012-03-12 09:16:43 +0000

    Fix Mobile Redirect by item_number_public
    git-svn-id: https://svn.localdev.cc/svn/shopgate_plugin_library/trunk@43 e652e40c-185c-4641-87fb-3ecfe257c510

--- vendors/mobile_redirect.class.php
+++ vendors/mobile_redirect.class.php
@@ -4,10 +4,10 @@ include_once dirname(__FILE__).'/../shopgate.php';
 
 /**
  * Manage and create the JavaScript for redirect to mobile-site
- * 
+ *
  * Sample:
  * <code>
- * &lt;!-- BEGIN SHOPGATE MOBILE HEADER CODE --&gt; 
+ * &lt;!-- BEGIN SHOPGATE MOBILE HEADER CODE --&gt;
  * &lt;script&gt;
  * var _shopgate = {};
  * _shopgate.shop_number = "10334";
@@ -18,7 +18,7 @@ include_once dirname(__FILE__).'/../shopgate.php';
  * &lt;/script&gt;
  * &lt;!-- END SHOPGATE MOBILE HEADER CODE --&gt;
  * </code>
- * 
+ *
  * @author Martin Weber (Shopgate GmbH)
  * @version 1.0.0
  * @see http://www.shopgate.com/forum/showthread.php?280-Einbinden-der-Mobilen-Webseite
@@ -28,118 +28,118 @@ class MobileRedirect {
 	const DST_ITEM		= "item";
 	const DST_CATEGORY	= "category";
 	const DST_CMS		= "cms";
-	
+
 	const HTTP_HOST		= "http://static.shopgate.com";
 	const HTTPS_HOST	= "https://static-ssl.shopgate.com";
-	
-	const HTTP_PG_HOST		= "http://static.shopgatepg.com";
+
+	const HTTP_PG_HOST	= "http://static.shopgatepg.com";
 	const HTTPS_PG_HOST	= "https://static-ssl.shopgatepg.com";
-	
+
 	/**
 	 * Use Https
 	 * @var boolean
 	 */
 	private $useHttps	= false;
-	
+
 	/**
 	 * Options to be set as json-array
 	 * @var array
 	 */
 	private $options	= array();
-	
+
 	/**
 	 * The configuration of the Framework
 	 * @var array
 	 */
 	private $config		= array();
-	
+
 	/**
 	 * The constructor load the Configuration and check the Request for using HTTPS
-	 * 
+	 *
 	 * The host and the shop_number will added options-array
 	 */
 	public function __construct() {
 		try {
 			$this->config = ShopgateConfig::validateAndReturnConfig();
 			$this->useHttps = isset($_SERVER["HTTPS"]) && ($_SERVER["HTTPS"] === "on" || $_SERVER["HTTPS"] == "1");
-			
+
 			$this->addOption("host", $this->__getHostUrl());
 			$this->addOption("shop_number", $this->config["shop_number"]);
 		} catch (Exception $e) {
-			
+
 		}
 	}
-	
+
 	/**
 	 * Return a open script-tag for JavaScript
-	 *  
+	 *
 	 * @param string $src the source of an external Script
 	 * @return string
 	 */
 	private function __jsOpen($src = "") {
-		
+
 		$jScript  = "<script type=\"text/javascript\"";
 		if(!empty($src)) $jScript .= " src=\"$src\"";
 		$jScript .= ">\n";
-		
+
 		return $jScript;
 	}
-	
+
 	/**
 	 * Close a script-tag
-	 * 
+	 *
 	 * @return string
 	 */
 	private function __jsClose() {
 		return "</script>\n";
 	}
-	
+
 	/**
 	 * Returend the URL to the Hos, where the js-files hostet and switch between HTTP and HTTPS
-	 * 
+	 *
 	 * @return string
 	 */
 	private function __getHostUrl() {
 		$url = $this->useHttps ? MobileRedirect::HTTPS_HOST : MobileRedirect::HTTP_HOST;
-		
+
 		if($this->config["server"] == "pg") $url = $this->useHttps ? MobileRedirect::HTTPS_PG_HOST : MobileRedirect::HTTP_PG_HOST;
-		
+
 		if(defined("SHOPGATE_DEBUG") && SHOPGATE_DEBUG && isset($this->config["aws_public"])) $url = $this->config["aws_public"];
-			
+
 		return $url;
 	}
-	
+
 	/**
 	 * Returned the URL to the js-File
-	 * 
+	 *
 	 * @return string
 	 */
 	private function __getJsUrl() {
 		return $this->__getHostUrl()
 			. "/mobile_header/".$this->config["shop_number"].".js";
 	}
-	
+
 	/**
 	 * Build a JavaScript-JSON-Object
-	 * 
+	 *
 	 * @param string $objName Name of the json-Object
 	 * @param array $objValues Key-Value-Pairs for Parameters
-	 * 
+	 *
 	 *  @return string
 	 */
 	private function __buildJsonOptions($objName, $objValues = array()) {
 		$json = "var $objName = {}\n";
-		
+
 		foreach($objValues as $key => $value) {
 			$json .= "$objName.$key = '$value';\n";
 		}
-		
+
 		return $json;
-	} 
-	
+	}
+
 	/**
 	 * Build the whole Script-block for redirectiong to mobile website
-	 * 
+	 *
 	 * @return string
 	 */
 	private function __render() {
@@ -150,21 +150,21 @@ class MobileRedirect {
 		$jScript .= "//]]>\n";
 // 		$jScript .= "-->\n";
 		$jScript .= $this->__jsClose();
-		
+
 		$jScript .= $this->__jsOpen($this->__getJsUrl());
 		$jScript .= $this->__jsClose();
-		
+
 		return $jScript;
 	}
-	
+
 	/**
-	 * Add Attribute to options. On rendering they will attach to the json-object 
-	 * 
+	 * Add Attribute to options. On rendering they will attach to the json-object
+	 *
 	 * <code>
 	 * var obj = {};
 	 * obj.key = 'value';
 	 * </code>
-	 * 
+	 *
 	 * @param string $key
 	 * @param string $value
 	 */
@@ -172,10 +172,10 @@ class MobileRedirect {
 		$key = trim($key);
 		$this->options[$key] = $value;
 	}
-	
+
 	/**
 	 * Remove a Attribute from Options
-	 * 
+	 *
 	 * @param string $key
 	 */
 	public function removeOption($key) {
@@ -183,12 +183,12 @@ class MobileRedirect {
 		if(array_key_exists($key, $this->options))
 			unset($this->options[$key]);
 	}
-	
+
 	/**
 	 * Return the rendered JavaScript for redirecting to mobile startpage
-	 * 
+	 *
 	 * If $directDraw ist set true the script will prompt with echo
-	 * 
+	 *
 	 * @param boolean $directDraw
 	 */
 	public function getScript($directDraw = false) {
@@ -196,49 +196,49 @@ class MobileRedirect {
 		if($directDraw) echo $jScript;
 		return $jScript;
 	}
-	
+
 	/**
 	 * Return the rendered JavaScript for redirecting to mobile categorypage
-	 * 
+	 *
 	 * If $directDraw ist set true the script will prompt with echo
-	 * 
+	 *
 	 * @param string $categoryNumber
 	 * @param boolean $directDraw
 	 */
 	public function getCategoryScript($categoryNumber, $directDraw = false) {
 		$this->addOption("redirect", MobileRedirect::DST_CATEGORY);
 		$this->addOption("category_number", $categoryNumber);
-		
+
 		$this->getScript($directDraw);
 	}
-	
+
 	/**
 	 * Return the rendered JavaScript for redirecting to mobile productpage
-	 * 
+	 *
 	 * If $directDraw ist set true the script will prompt with echo
-	 * 
+	 *
 	 * @param string $itemId
 	 * @param boolean $directDraw
 	 */
 	public function getItemScript($itemId, $directDraw = false) {
 		$this->addOption("redirect", MobileRedirect::DST_ITEM);
 		$this->addOption("item_number", $itemId);
-		
+
 		$this->getScript($directDraw);
 	}
-	
+
 	/**
 	 * Return the rendered JavaScript for redirecting to mobile cmspage
-	 * 
+	 *
 	 * If $directDraw ist set true the script will prompt with echo
-	 * 
+	 *
 	 * @param string $categoryNumber
 	 * @param boolean $directDraw
 	 */
 	public function getCmsScript($categoryNumber, $directDraw = false) {
 		$this->addOption("redirect", MobileRedirect::DST_CMS);
 		$this->addOption("cms_page", $this->objId);
-		
+
 		$this->getScript($directDraw);
 	}
 }
