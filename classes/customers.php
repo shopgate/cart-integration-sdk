0670eab0ad0fbc9ae9fe3c569631e6e35d66c5a5
mueller <mueller@e652e40c-185c-4641-87fb-3ecfe257c510>
2012-03-19 07:30:46 +0000

    Fix, damit Adressen richtig zurückgegeben werden
    git-svn-id: https://svn.localdev.cc/svn/shopgate_plugin_library/trunk@62 e652e40c-185c-4641-87fb-3ecfe257c510

--- classes/customers.php
+++ classes/customers.php
@@ -176,8 +176,11 @@ class ShopgateCustomer extends ShopgateContainer {
 	public function getAddresses($type = ShopgateAddress::BOTH) {
 		$addresses = array();
 		
-		foreach ($this->addresses as $address)
-			if ($address->getAddressType() == $type) $addresses[] = $address;
+		foreach ($this->addresses as $address){
+			if (($address->getAddressType() & $type) == $address->getAddressType()) {
+				$addresses[] = $address;
+			}
+		}
 		
 		return $addresses;
 	}
