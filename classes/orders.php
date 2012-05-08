5c100f0011fe273e8e0696a5eb2d07cc854be591
ebel <ebel@e652e40c-185c-4641-87fb-3ecfe257c510>
2012-05-08 12:38:43 +0000

    $update_payment property went missing on last change. It's back now.
    git-svn-id: https://svn.localdev.cc/svn/shopgate_plugin_library/trunk@157 e652e40c-185c-4641-87fb-3ecfe257c510

--- classes/orders.php
+++ classes/orders.php
@@ -58,9 +58,18 @@ class ShopgateOrder extends ShopgateContainer {
 	protected $delivery_notes;
 	
 	/**
-	 * Status for updates
+	 * Set to true if shipping information should be updated.
+	 * 
+	 * @var bool
 	 */
 	protected $update_shipping = false;
+	
+	/**
+	 * Set to true if payment information should be updated.
+	 * 
+	 * @var bool
+	 */
+	protected $update_payment = false;
 
 
 	/**********
