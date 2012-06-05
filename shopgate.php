fbba151ae9a858c1267cbb0667a4527ea9dc8fda
weber <weber@e652e40c-185c-4641-87fb-3ecfe257c510>
2012-06-05 05:50:48 +0000

    Clean up shopgate.php
    git-svn-id: https://svn.localdev.cc/svn/shopgate_plugin_library/trunk@224 e652e40c-185c-4641-87fb-3ecfe257c510

--- shopgate.php
+++ shopgate.php
@@ -1,27 +1,18 @@
 <?php
 if (!defined('DS')) define('DS', '/');
 
+// Library
 require_once(dirname(__FILE__).DS.'classes'.DS.'core.php');
 require_once(dirname(__FILE__).DS.'classes'.DS.'customers.php');
 require_once(dirname(__FILE__).DS.'classes'.DS.'orders.php');
 require_once(dirname(__FILE__).DS.'classes'.DS.'redirect.php');
 
+// Shopgate-Vendors
 require_once(dirname(__FILE__).DS.'vendors'.DS.'2d_is.php');
-require_once(dirname(__FILE__).DS.'vendors'.DS.'JSON.php');
 require_once(dirname(__FILE__).DS.'vendors'.DS.'mobile_redirect.class.php');
 
 include_once dirname(__FILE__).DS.'vendors'.DS.'shopgate_phpqrcode'.DS.'qrlib.php';
 include_once dirname(__FILE__).DS.'vendors'.DS.'qr_code_manager.class.php';
 
-// try {
-// 	$config = ShopgateConfig::validateAndReturnConfig();
-
-// 	$plugin = $config["plugin"];
-
-// 	if(file_exists(SHOPGATE_BASE_DIR.'/plugins/'.$plugin)
-// 	&& file_exists(SHOPGATE_BASE_DIR.'/plugins/'.$plugin.'/includes.php')) {
-// 		include_once SHOPGATE_BASE_DIR.'/plugins/'.$plugin.'/includes.php';
-// 	}
-
-// } catch (Exception $e) {
-// }
+// External-Vendors
+if(!class_exists("Services_JSON")) include_once(dirname(__FILE__).DS.'vendors'.DS.'JSON.php');
