45b9eb613f5d839f4bbf6eb08f003fe9186e8897
plugin_jenkins <plugin_jenkins@e652e40c-185c-4641-87fb-3ecfe257c510>
2013-08-01 12:36:44 +0000

    Increased Shopgate Library to version 2.2.1.
    git-svn-id: https://svn.localdev.cc/svn/shopgate_plugin_library/trunk@1036 e652e40c-185c-4641-87fb-3ecfe257c510

--- build_config.php
+++ build_config.php
@@ -2,13 +2,13 @@
 $buildConfig = array (
 	'major' => 2,
 	'minor' => 2,
-	'build' => 0,
+	'build' => 1,
 	'shopgate_library_path' => "",
 	'plugin_name' => "library",
 	'display_name' => "Shopgate Library",
 	'zip_filename' => "shopgate_library.zip",
 	'version_files' => array (
-		0 => array (
+		'0' => array (
 			'path' => "/classes/core.php",
 			'match' => "/define\(\'SHOPGATE_LIBRARY_VERSION\',(.+)\)/",
 			'replace' => "define('SHOPGATE_LIBRARY_VERSION', '{PLUGIN_VERSION}')",
--- classes/core.php
+++ classes/core.php
@@ -3,7 +3,7 @@
 ###################################################################################
 # define constants
 ###################################################################################
-define('SHOPGATE_LIBRARY_VERSION', '2.2.0');
+define('SHOPGATE_LIBRARY_VERSION', '2.2.1');
 define('SHOPGATE_LIBRARY_ENCODING' , 'UTF-8');
 define('SHOPGATE_BASE_DIR', realpath(dirname(__FILE__).'/../'));
 
