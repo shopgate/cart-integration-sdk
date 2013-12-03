5ae4ce9bb966b89e4c82da96c0336bb26b618b47
mueller <mueller@e652e40c-185c-4641-87fb-3ecfe257c510>
2013-12-03 10:06:19 +0000

    build_config version changed to 2.4.-1
    git-svn-id: https://svn.localdev.cc/svn/shopgate_plugin_library/trunk@1165 e652e40c-185c-4641-87fb-3ecfe257c510

--- build_config.php
+++ build_config.php
@@ -1,8 +1,8 @@
 <?php
 $buildConfig = array (
 	'major' => 2,
-	'minor' => 3,
-	'build' => 10,
+	'minor' => 4,
+	'build' => -1,
 	'shopgate_library_path' => "",
 	'plugin_name' => "library",
 	'display_name' => "Shopgate Library 2.3.x",
