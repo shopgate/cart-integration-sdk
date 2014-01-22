d1cb7d1ae131fbdf27b253681f122e43b6b145df
plugin_jenkins <plugin_jenkins@e652e40c-185c-4641-87fb-3ecfe257c510>
2014-01-22 08:45:41 +0000

    Increased Shopgate Library 2.4.x to version 2.4.4.
    git-svn-id: https://svn.localdev.cc/svn/shopgate_plugin_library/trunk@1212 e652e40c-185c-4641-87fb-3ecfe257c510

--- build_config.php
+++ build_config.php
@@ -2,7 +2,7 @@
 $buildConfig = array (
 	'major' => 2,
 	'minor' => 4,
-	'build' => 3,
+	'build' => 4,
 	'shopgate_library_path' => "",
 	'plugin_name' => "library",
 	'display_name' => "Shopgate Library 2.4.x",
