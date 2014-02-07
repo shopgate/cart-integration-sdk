fcb2ae09edfb214c7467b008235cb9169999f1a7
plugin_jenkins <plugin_jenkins@e652e40c-185c-4641-87fb-3ecfe257c510>
2014-02-07 14:46:15 +0000

    Increased Shopgate Library 2.4.x to version 2.4.9.
    git-svn-id: https://svn.localdev.cc/svn/shopgate_plugin_library/trunk@1245 e652e40c-185c-4641-87fb-3ecfe257c510

--- classes/core.php
+++ classes/core.php
@@ -24,7 +24,7 @@
 ###################################################################################
 # define constants
 ###################################################################################
-define('SHOPGATE_LIBRARY_VERSION', '2.4.7');
+define('SHOPGATE_LIBRARY_VERSION', '2.4.9');
 define('SHOPGATE_LIBRARY_ENCODING' , 'UTF-8');
 define('SHOPGATE_BASE_DIR', realpath(dirname(__FILE__).'/../'));
 
