4529302dedac34a0faa19c7cce04bf95c3bd30f2
rutkowski <rutkowski@e652e40c-185c-4641-87fb-3ecfe257c510>
2014-03-03 15:03:59 +0000

    Neue protected Methode "arrayCross" welches ein zwei-dimensionales Array annimmt und iterativ ein Kreuzprodukt bildet, welches z.B. dazu geeignet ist Kind-Produkte anhand von mehreren Optionen und Optionswerten pro Produkt zu generieren (die Methode selbst erzeugt in dem Fall alle Kombinationen zu möglichen Optionen und deren Optionswerten und liefert dann ein zweidimensionales Array zurück - tiefere Dimensionsebenen werden als Einheit betrachtet).
    git-svn-id: https://svn.localdev.cc/svn/shopgate_plugin_library/trunk@1269 e652e40c-185c-4641-87fb-3ecfe257c510

--- classes/core.php
+++ classes/core.php
@@ -1929,6 +1929,72 @@ abstract class ShopgatePlugin extends ShopgateObject {
 	 * @throws ShopgateLibraryException
 	 */
 	//protected abstract function getPagesCsv();
+
+	/**
+	 * Takes an array of arrays that contain all elements which are taken to create a cross-product of all elements. The resulting array is an array-list with
+	 * each possible combination as array. An Element itself can be anything (including a whole array that is not torn apart, but instead treated as a whole)
+	 * By setting the second parameter to true, the keys of the source array is added as an array at the front of the resulting array
+	 *
+	 * Sample input: array(
+	 * 		'group-1-key' => array('a', 'b'),
+	 * 		'group-2-key' => array('x'),
+	 * 		7 => array('l', 'm', 'n'),
+	 * );
+	 * Output of sample: Array (
+	 * 		[0] => Array (
+	 * 			[group-1-key] => a
+	 * 			[group-2-key] => x
+	 * 			[7] => l
+	 * 		)
+	 * 		[1] => Array (
+	 * 			[group-1-key] => b
+	 * 			[group-2-key] => x
+	 * 			[7] => l
+	 * 		)
+	 * 		[2] => Array (
+	 * 			[group-1-key] => a
+	 * 			[group-2-key] => x
+	 * 			[7] => m
+	 * 		)
+	 * 		[...] and so on ... (total of count(src[0])*count(src[1])*...*count(src[N]) elements) [=> 2*1*3 elements in this case]
+	 * 	)
+	 *
+	 * @param array $src: The (at least) double dimensioned array input
+	 * @param string $enableFirstRow: Disabled by default
+	 * @return array[][]:
+	 */
+	protected function arrayCross(array $src, $enableFirstRow = false) {
+		$result = array();
+		$firstRow = array();
+		
+		if($enableFirstRow) {
+			$firstRow[0] = array_keys($src);
+		}
+		
+		foreach($src as $key => $valArr) {
+			// elements are copied for appending data, so the actual count is needed as the base-element-count
+			$copyCount = count($result);
+			
+			// start by using the first array as a resultset (in case of only one array the result of the cross-product is the first input-array)
+			if(empty($result)) {
+				foreach($valArr as $optionSelection) {
+					$result[] = array($key => $optionSelection);
+				}
+			} else {
+				$i = 0;
+				foreach($valArr as $optionSelection) {
+					for($j = 0; $j < $copyCount; $j++) {
+						// in case of $i==0 it copies itself, so it's correct in all cases if $i
+						$result[$i*$copyCount+$j] = $result[$j];
+						$result[$i*$copyCount+$j][$key] = $optionSelection;
+					}
+					$i++;
+				}
+			}
+		}
+		
+		return array_merge($firstRow, $result);
+	}
 }
 
 interface ShopgateFileBufferInterface {
