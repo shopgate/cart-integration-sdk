37480c7c04d61405b6da460f6fbea1d4553560e6
Timo Ebel <timo.ebel@shopgate.com>
2014-07-22 10:47:06 +0200

    Added external_orders.php to includes.

--- shopgate.php
+++ shopgate.php
@@ -32,6 +32,7 @@ require_once(dirname(__FILE__).DS.'classes'.DS.'apis.php');
 require_once(dirname(__FILE__).DS.'classes'.DS.'configuration.php');
 require_once(dirname(__FILE__).DS.'classes'.DS.'customers.php');
 require_once(dirname(__FILE__).DS.'classes'.DS.'orders.php');
+require_once(dirname(__FILE__).DS.'classes'.DS.'external_orders.php');
 require_once(dirname(__FILE__).DS.'classes'.DS.'items.php');
 require_once(dirname(__FILE__).DS.'classes'.DS.'redirect.php');
 
