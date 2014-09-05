e9cd4e7fa5e739f66e42bdc9e5a2cb65231be30e
Peter Liebig <p.liebig@me.com>
2014-09-05 15:16:52 +0200

    https://shopgate.atlassian.net/browse/LIBRARY-1 changelog and build_config

--- build_config.php
+++ build_config.php
@@ -1,11 +1,11 @@
 <?php
 $buildConfig = array (
 	'major' => 2,
-	'minor' => 8,
-	'build' => 10,
+	'minor' => 9,
+	'build' => -1,
 	'shopgate_library_path' => "",
 	'plugin_name' => "library",
-	'display_name' => "Shopgate Library 2.8.x",
+	'display_name' => "Shopgate Library 2.9.x",
 	'zip_filename' => "shopgate_library.zip",
 	'version_files' => array (
 		'0' => array (
