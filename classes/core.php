10cae17c26dcd9a2fa0992bc87bd63647b677a77
interface-jenkins <jenkins@shopgate.com>
2014-10-30 17:38:49 +0100

    Increased Shopgate Library 2.9.x to version 2.9.4.

--- classes/core.php
+++ classes/core.php
@@ -24,7 +24,7 @@
 ###################################################################################
 # define constants
 ###################################################################################
-define('SHOPGATE_LIBRARY_VERSION', '2.9.3');
+define('SHOPGATE_LIBRARY_VERSION', '2.9.4');
 define('SHOPGATE_LIBRARY_ENCODING' , 'UTF-8');
 define('SHOPGATE_BASE_DIR', realpath(dirname(__FILE__).'/../'));
 
