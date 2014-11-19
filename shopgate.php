77bc61e45196efee3fd3e3cadd28655ff1593985
Stephan Recknagel <stephan.recknagel@shopgate.com>
2014-11-19 13:28:55 +0100

    ticketless - remove unused vendor include due to risk of double declaration of class MobileRedirect on merchant server

--- shopgate.php
+++ shopgate.php
@@ -74,7 +74,6 @@ require_once(dirname(__FILE__).DS.'classes'.DS.'models/media/Image.php');
 
 // Shopgate-Vendors
 require_once(dirname(__FILE__).DS.'vendors'.DS.'2d_is.php');
-require_once(dirname(__FILE__).DS.'vendors'.DS.'mobile_redirect.class.php');
 
 // External-Vendors
 include_once(dirname(__FILE__).DS.'vendors'.DS.'JSON.php');
