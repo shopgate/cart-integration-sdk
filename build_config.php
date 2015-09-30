65bf22a8e94cccdd4d5cc712b9316871f0606f64
interface-jenkins <jenkins@shopgate.com>
2015-09-30 15:01:53 +0200

    Increased Shopgate Library 2.9.x to version 2.9.29.

--- build_config.php
+++ build_config.php
@@ -2,7 +2,7 @@
 $buildConfig = array (
 	'major' => 2,
 	'minor' => 9,
-	'build' => 28,
+	'build' => 29,
 	'shopgate_library_path' => "",
 	'plugin_name' => "library",
 	'display_name' => "Shopgate Library 2.9.x",
