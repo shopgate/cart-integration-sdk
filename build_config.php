9a5a54e39c04547dade162426658e55e008ebb1b
interface-jenkins <jenkins@shopgate.com>
2016-03-31 11:20:19 +0200

    Increased Shopgate Library 2.9.x to version 2.9.42.

--- build_config.php
+++ build_config.php
@@ -2,33 +2,33 @@
 $buildConfig = array (
 	'major' => 2,
 	'minor' => 9,
-	'build' => 41,
-	'shopgate_library_path' => "",
-	'plugin_name' => "library",
-	'display_name' => "Shopgate Library 2.9.x",
-	'zip_filename' => "shopgate_library.zip",
+	'build' => 42,
+	'shopgate_library_path' => '',
+	'plugin_name' => 'library',
+	'display_name' => 'Shopgate Library 2.9.x',
+	'zip_filename' => 'shopgate_library.zip',
 	'version_files' => array (
 		'0' => array (
-			'path' => "/classes/core.php",
+			'path' => '/classes/core.php',
 			'match' => '/define\("SHOPGATE_LIBRARY_VERSION",(.+)\)/',
 			'replace' => 'define("SHOPGATE_LIBRARY_VERSION", "{PLUGIN_VERSION}")',
 		),
 	),
 	'wiki' => array (
 		'changelog' => array (
-			'path' => "./",
+			'path' => './',
 			'pages' => array (
 				'ShopgateLibrary' => array (
-					'title' => "Template:Shopgate_Library_Changelog",
+					'title' => 'Template:Shopgate_Library_Changelog',
 					'languages' => array (
-						'0' => "English",
-						'1' => "Deutsch",
+						'0' => 'English',
+						'1' => 'Deutsch',
 					),
 				),
 			),
 		),
 	),
-	'zip_basedir' => "shopgate_library",
+	'zip_basedir' => 'shopgate_library',
 	'exclude_files' => array (
 	),
 );
