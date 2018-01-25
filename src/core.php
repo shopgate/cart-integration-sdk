bcb18ed238164722af7642304cf33cdd1241ff53
André Kraus <andre.kraus@shopgate.com>
2018-01-25 12:09:00 +0100

    Updated version constant

--- src/core.php
+++ src/core.php
@@ -22,7 +22,7 @@
 ###################################################################################
 # define constants
 ###################################################################################
-define("SHOPGATE_LIBRARY_VERSION", "2.9.71");
+define("SHOPGATE_LIBRARY_VERSION", "2.9.72");
 define('SHOPGATE_LIBRARY_ENCODING', 'UTF-8');
 define('SHOPGATE_BASE_DIR', realpath(dirname(__FILE__) . '/../'));
 
